883cb21cac3ac3b2ec965ba8088d6357262f9664
kvn-media <kevin.subagio@gmail.com>
2024-05-04 19:38:15 +0700

    this repo dedicated for learning Julia languages

---
+++ integer_and_float_num.jl
@@ -0,0 +1,8 @@
+println(1)
+
+typeof(1)
+println(typeof)
+
+println(Sys.WORD_SIZE)
+
+println(typeof(3000000000))
---
+++ variabel.jl
@@ -0,0 +1,44 @@
+x = 10
+println(x + 1)
+
+x = 1 + 1
+println(x)
+
+x = "Hello World!"
+println(x)
+
+x = 1.0
+println(x)
+
+y = -3
+println(y)
+
+z = "My String"
+println(z)
+
+customary_phrase = "Hello world!"
+println(customary_phrase)
+
+UniversalDeclarationOfHumanRightsStart = ""
+println(UniversalDeclarationOfHumanRightsStart)
+
+δ = 0.00001
+println(δ)
+
+pi = 3
+println(pi)
+
+sqrt = 4
+println(sqrt)
+
+a = (b = 2+2) + 3
+println(a)
+
+a = [1,2,3]
+println(a)
+
+b = a
+println(b)
+
+a[1] = 42
+println(a)
